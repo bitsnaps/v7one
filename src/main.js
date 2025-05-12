cb6d5c338890668ae06a6a2120a9fb6ee9bf06ba
Ibrahim H <bitsnaps@yahoo.fr>
2025-05-12 10:54:25 +0100

    feat(ui): change default theme

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import { createApp } from 'vue'
 import { createBootstrap } from 'bootstrap-vue-next'
 // Add the necessary CSS
-import 'bootstrap/dist/css/bootstrap.css'
+// import 'bootstrap/dist/css/bootstrap.css' // Default Bootstrap
+import './assets/css/bootstrap.min.css' // Customized Boostswatch (Minty)
 import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
 import './style.css'
 import App from './App.vue'
