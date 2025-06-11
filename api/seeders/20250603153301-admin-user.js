9d499fe25328c0b239f4d71bd28798e544c90f43
Ibrahim H <bitsnaps@yahoo.fr>
2025-06-11 20:12:13 +0100

    fix(seeders): standardize enum values

--- api/seeders/20250603153301-admin-user.js
+++ api/seeders/20250603153301-admin-user.js
@@ -1,11 +1,12 @@
 'use strict';
+const crypto = require('crypto');
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
     await queryInterface.bulkInsert('Users', [
       {
-        id: '0d827236-f4cc-4a86-b55a-024a7723ae5c',
+        id: crypto.randomUUID(),
         displayName: 'admin',
         email: 'admin@example.com',
         passwordHash: '8408dd9c2311bd608517bd62827439a8:16443aa8eb03758e2b9866e562f14cc101182b6463b4798a09a926f50da26abe1bba2e53a7691aab075c5fd8338f20c8e23292fb7d5c668880bd42e755ba3153',
